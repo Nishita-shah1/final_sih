e98bd74a6051e4c028a756c9e5a53064dce89423
Nishita-shah1 <nishita182005@gmail.com>
2024-12-04 02:18:51 +0530

    public folder added

--- src/app/(app)/maps/page.tsx
+++ src/app/(app)/maps/page.tsx
@@ -1,19 +1,13 @@
 'use client';
 
-import React, { useState } from 'react';
+import React from 'react';
 
-export default function Maps() {
+const maps: React.FC = () => {
   return (
-    <div className="container mx-auto p-6">
-      {/* Maps in Progress Message */}
-      <div className="text-center text-xl font-bold text-gray-600">
-        Maps in progress...
-      </div>
-
-      {/* Fish Gallery */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {/* Gallery content will go here */}
-      </div>
+    <div>
+      <p>maps in In Progress...</p>
     </div>
   );
 }
+
+export default maps;
